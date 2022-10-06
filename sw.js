981a03253c2ea4e8bc39b4be5704c48e50db79ca
yorvich <wilsonagu@gmail.com>
2022-10-05 20:47:40 -0500

    Eliminadas en el sw las referencias a la raís ('/')

--- sw.js
+++ sw.js
@@ -12,15 +12,15 @@ const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 const APP_SHELL=[
     // '/',
     //Solo es necesario estrictamente para el ambiente de desarrollo 
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
     'js/sw-utils.js'
 ];
 
@@ -30,8 +30,8 @@ const APP_SHELL_INMUTABLE=[
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    '/css/animate.css',
-    '/js/libs/jquery.js'
+    'css/animate.css',
+    'js/libs/jquery.js'
 ];
 
 
