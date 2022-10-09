a41ea37a6b5dab562c39173317cbd1d6e1b070d1
yorvich <wilsonagu@gmail.com>
2022-10-09 16:15:03 -0500

    Modificaciones en el sw y el index para trabajar con nueva versión de fontawesome

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ const APP_SHELL=[
 const APP_SHELL_INMUTABLE=[
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/fontawesome-free-6.2.0-web-all.css',
     'css/animate.css',
     'js/libs/jquery.js'
 ];
